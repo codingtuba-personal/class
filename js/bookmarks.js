236765d7b105d3964ce871be88ac6fe696d91738
Cade McNair <27cadem@MS-27cadem.local>
2022-03-09 16:23:03 +0700

    class 1

---
+++ js/bookmarks.js
@@ -0,0 +1,5 @@
+document.querySelector("input.bookmarks-input").onkeyup=(event)=>{
+    if(event.code=="Enter"){
+        alert("You pushed enter!")
+    }
+}
---
+++ js/index.js
@@ -0,0 +1,3 @@
+document.querySelector("button.button-1").onclick=function(){
+    location.replace("bookmarks.html")
+}
