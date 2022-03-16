64dbd828097efea9feeeaeb0537538a7d8af74e3
Cade McNair <27cadem@MS-27cadem.local>
2022-03-16 16:24:45 +0700

    class-2-future

--- js/bookmarks.js
+++ js/bookmarks.js
@@ -1,5 +1,26 @@
+if(!localStorage.getItem("quick-bookmarks-list")){localStorage.setItem("quick-bookmarks-list",JSON.stringify([]))}
+let bookmarks=JSON.parse(localStorage.getItem("quick-bookmarks-list"))
+
+bookmarks.forEach((i,d)=>{
+    let li=document.createElement("li")
+    li.innerHTML=`<button class="delete-bookmark" data-id="${d}">-</button> <a href="${i}" target="_blank" class="bookmark-url">${i}</a>`
+    li.classList.add("bookmark")
+
+    document.querySelector("ul.all-bookmarks").appendChild(li)
+})
+
+document.querySelectorAll(".delete-bookmark").forEach(e=>{
+    e.onclick=()=>{
+        bookmarks.splice(e.getAttribute("data-id")-0,1)
+        localStorage.setItem("quick-bookmarks-list",JSON.stringify(bookmarks))
+        location.reload()
+    }
+})
+
 document.querySelector("input.bookmarks-input").onkeyup=(event)=>{
     if(event.code=="Enter"){
-        alert("You pushed enter!")
+        bookmarks.push(document.querySelector("input.bookmarks-input").value)
+        localStorage.setItem("quick-bookmarks-list",JSON.stringify(bookmarks))
+        location.reload()
     }
 }
